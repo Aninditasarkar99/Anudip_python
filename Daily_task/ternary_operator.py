8214b86b8495422a9cd572e5beb091cc5837a673
Anindita Sarkar <sarkaranindita55@gmail.com>
2023-11-01 22:12:56 +0530

    2nd task

--- Daily_task/ternary_operator.py
+++ Daily_task/ternary_operator.py
@@ -1,6 +1,6 @@
-#Declare two variables and print that which variable is largest using ternary
-#operators
-#01/11/2023
+# Declare two variables and print that which variable is largest using ternary
+# operators
+# 01/11/2023
 
 first_vari = 40
 second_vari = 34
@@ -8,3 +8,5 @@ if first_vari > second_vari:
     print("Largest number is  ", first_vari)
 else:
     print("Largest number is  ", second_vari)
+
+# Output - Largest number is   40
---
+++ Daily_task/triangle.py
@@ -0,0 +1,14 @@
+# Python program to find the area of a triangle whose sides are given
+
+a = 5
+b = 6
+c = 7
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area_of_triangle = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f'%area_of_triangle)
+
+# output -- The area of the triangle is 14.70
