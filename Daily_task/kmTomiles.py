0fba972a70d2edd67920ae12d3596ac03412f351
Anindita Sarkar <sarkaranindita55@gmail.com>
2023-11-02 20:48:41 +0530

    3rd task

---
+++ Daily_task/kmTomiles.py
@@ -0,0 +1,13 @@
+# Python Program to Convert Kilometers to Miles
+km = float(input("Enter the distance in kilometers: "))
+
+fact = 0.621371
+
+miles = km * fact
+
+print("%.2f kilometers is equal to %.2f miles" %(km, miles))
+
+'''OUTPUT - 
+Enter the distance in kilometers: 5
+5.00 kilometers is equal to 3.11 miles
+'''
