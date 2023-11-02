fc10eaaff50af50ba9e4b10dd9cbbe2f4708fb98
Anindita Sarkar <sarkaranindita55@gmail.com>
2023-11-02 20:56:50 +0530

    4th task

---
+++ Daily_task/Simple Interest.py
@@ -0,0 +1,12 @@
+# Find the Simple Interest on Rs. 200 for 5 years at 5% per year.
+
+Anual = 200
+time = 5
+rate = 5
+
+simple_interest = (Anual * time * rate) / 100
+print("The Calculation Of simple Interest is " , simple_interest)
+
+''' OUTPUT - 
+The Calculation Of simple Interest is  50.0
+'''
